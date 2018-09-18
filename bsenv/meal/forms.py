41a5600c808a63ad082aba6ed1185eb522e2cc03
shahimran <shahimrancoxbd@gmail.com>
2018-09-18 23:50:37 +0600

    form data sent to database

---
+++ bsenv/meal/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Token
+class TokenForm(forms.ModelForm):
+
+    class Meta:
+        model = Token
+        fields = ['empName', 'empId',]
--- bsenv/meal/views.py
+++ bsenv/meal/views.py
@@ -5,19 +5,21 @@ from .forms import TokenForm
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
 def menu(request):
     return render(request,"menu.html")
+
 def token_list(request):
     tokens = Token.objects.all()
     return render(request, "token_list.html", {'tokens': tokens})
+
 def registration(request):
-    # if request.method == "POST":
-    #     form = TokenForm(request.POST)
-    #     if form.is_valid():
-    #         token = form.save(commit=False)
-    #         token.empName = request.user
-    #         token.empId = request.user
-    #         token.meal_date = timezone.now()
-    #         token.save()
-    #     return redirect('/')
-    return render(request, 'registration.html')
+	form = TokenForm()
+	if request.method == 'POST':
+		form = TokenForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print("error! check model ,view & form file.")
+	return render(request,'registration.html',{'form':form})
